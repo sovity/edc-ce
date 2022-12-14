a3d93c2c789e3dd695a251e2a1b59c289fcac900
Tim Berthold <tim.berthold@sovity.de>
2022-12-14 13:43:30 +0100

    chore: add init files

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,3 +70,21 @@ allprojects {
 //        }
     }
 }
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/sovity/broker-extension")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
