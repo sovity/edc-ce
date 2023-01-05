9c56c1b6a29e2032068e5946fd34e5a4f855bd90
omar <omar.silva@sovity.de>
2023-01-05 11:34:41 +0100

    chore: add policy template for connector restricted usage

--- src/modules/edc-demo/components/new-policy-dialog/new-policy-dialog.component.ts
+++ src/modules/edc-demo/components/new-policy-dialog/new-policy-dialog.component.ts
@@ -34,16 +34,27 @@ export class NewPolicyDialogComponent implements OnInit {
 
   onSave() {
     this.policyDefinition.id = this.policyDefinition.id.trim()
-    const permissionTemplate: string = "{    \"edctype\": \"dataspaceconnector:permission\",    \"id\": null,    \"target\": \"urn:artifact:urn:artifact:bitcoin\",    \"action\": {      \"type\": \"USE\",      \"includedIn\": null,      \"constraint\": null    },    \"assignee\": null,    \"assigner\": null,    \"constraints\": [      {        \"edctype\": \"AtomicConstraint\",        \"leftExpression\": {          \"edctype\": \"dataspaceconnector:literalexpression\",          \"value\": \"POLICY_EVALUATION_TIME\"        },        \"operator\": \"GT\",        \"rightExpression\": {          \"edctype\": \"dataspaceconnector:literalexpression\",          \"value\": \"2022-08-31T00:00:00.001Z\"        }      },      {        \"edctype\": \"AtomicConstraint\",        \"leftExpression\": {          \"edctype\": \"dataspaceconnector:literalexpression\",          \"value\": \"POLICY_EVALUATION_TIME\"        },        \"operator\": \"LT\",        \"rightExpression\": {          \"edctype\": \"dataspaceconnector:literalexpression\",          \"value\": \"2023-08-31T23:59:59.000Z\"        }      }    ],    \"duties\": []  }";
-    let permission = JSON.parse(permissionTemplate);
-    let constraints = permission["constraints"];
-    let startDateConstraint = constraints[0]
-    let endDateConstraint = constraints[1]
-    let startDate : Date = this.range.value["start"] as Date
-    let endDate : Date = this.range.value["end"] as Date
-    startDateConstraint["rightExpression"]["value"] = startDate.toISOString();
-    endDateConstraint["rightExpression"]["value"] = endDate.toISOString();
-    this.policy.permissions = [permission]
+
+    if (this.policyType === "Time-Period-Restricted") {
+      const permissionTemplate: string = "{    \"edctype\": \"dataspaceconnector:permission\",    \"id\": null,    \"target\": \"urn:artifact:urn:artifact:bitcoin\",    \"action\": {      \"type\": \"USE\",      \"includedIn\": null,      \"constraint\": null    },    \"assignee\": null,    \"assigner\": null,    \"constraints\": [      {        \"edctype\": \"AtomicConstraint\",        \"leftExpression\": {          \"edctype\": \"dataspaceconnector:literalexpression\",          \"value\": \"POLICY_EVALUATION_TIME\"        },        \"operator\": \"GT\",        \"rightExpression\": {          \"edctype\": \"dataspaceconnector:literalexpression\",          \"value\": \"2022-08-31T00:00:00.001Z\"        }      },      {        \"edctype\": \"AtomicConstraint\",        \"leftExpression\": {          \"edctype\": \"dataspaceconnector:literalexpression\",          \"value\": \"POLICY_EVALUATION_TIME\"        },        \"operator\": \"LT\",        \"rightExpression\": {          \"edctype\": \"dataspaceconnector:literalexpression\",          \"value\": \"2023-08-31T23:59:59.000Z\"        }      }    ],    \"duties\": []  }";
+      let permission = JSON.parse(permissionTemplate);
+      let constraints = permission["constraints"];
+      let startDateConstraint = constraints[0]
+      let endDateConstraint = constraints[1]
+      let startDate: Date = this.range.value["start"] as Date
+      let endDate: Date = this.range.value["end"] as Date
+      startDateConstraint["rightExpression"]["value"] = startDate.toISOString();
+      endDateConstraint["rightExpression"]["value"] = endDate.toISOString();
+      this.policy.permissions = [permission]
+    } else if (this.policyType === 'Connector-Restricted-Usage') {
+      const permissionTemplate: string = "{\"edctype\":\"dataspaceconnector:permission\",\"id\":null,\"target\":\"urn:artifact:urn:artifact:bitcoin\",\"action\":{\"type\":\"USE\",\"includedIn\":null,\"constraint\":null},\"assignee\":null,\"assigner\":null,\"constraints\":[{\"edctype\":\"AtomicConstraint\",\"leftExpression\":{\"edctype\":\"dataspaceconnector:literalexpression\",\"value\":\"REFERRING_CONNECTOR\"},\"operator\":\"EQ\",\"rightExpression\":{\"edctype\":\"dataspaceconnector:literalexpression\",\"value\":\"http://example.org\"}}],\"duties\":[]}\n"
+      let permission = JSON.parse(permissionTemplate);
+      let constraints = permission["constraints"];
+      let connectorConstraint = constraints[0]
+      connectorConstraint["rightExpression"]["value"] = this.connectorId as string;
+      this.policy.permissions = [permission]
+    }
+
     this.dialogRef.close({
       policy: this.policyDefinition.policy,
       id: this.policyDefinition.id
