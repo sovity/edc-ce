761b42276d43f1f814c6bc9739fc16dc710504e6
efiege <105237007+efiege@users.noreply.github.com>
2023-01-06 15:15:24 +0100

    feat: publish maven artifacts for extensions (#5)
    Co-authored-by: Tim Berthold <75306992+tmberthold@users.noreply.github.com>

--- extensions/event-asset-provider/build.gradle.kts
+++ extensions/event-asset-provider/build.gradle.kts
@@ -1,13 +1,27 @@
 plugins {
     `java-library`
+    `maven-publish`
 }
-tasks.register("prepareKotlinBuildScriptModel"){}
+
 val edcVersion: String by project
 val edcGroup: String by project
+val theGroup: String by project
+val theVersion: String by project
+
+group = theGroup
+version = theVersion
 
 dependencies {
     implementation("${edcGroup}:core-spi:${edcVersion}")
     implementation("${edcGroup}:contract-spi:${edcVersion}")
     implementation("${edcGroup}:control-plane-spi:${edcVersion}")
     implementation(project(":extensions:catalog-transfer-extension"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>(project.name) {
+            from(components["java"])
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,4 @@
 rootProject.name = "broker-extension"
-include("extensions:policies")
-findProject(":extensions:policies")?.name = "policies"
 include("extensions:catalog-transfer-extension")
 findProject(":extensions:catalog-transfer-extension")?.name = "catalog-transfer-extension"
 include("extensions:event-asset-provider")
