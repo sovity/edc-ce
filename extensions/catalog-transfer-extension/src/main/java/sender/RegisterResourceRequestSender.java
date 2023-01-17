7e890056ddea7ab327dbe96f85861341e5bd0df7
omar <omar.silva@sovity.de>
2023-01-17 13:58:02 +0100

    chore: add geo reference method to resource payload
    refactor: method names

--- extensions/catalog-transfer-extension/src/main/java/sender/RegisterResourceRequestSender.java
+++ extensions/catalog-transfer-extension/src/main/java/sender/RegisterResourceRequestSender.java
@@ -29,7 +29,6 @@ import org.eclipse.edc.protocol.ids.api.multipart.dispatcher.sender.response.Ids
 import org.eclipse.edc.protocol.ids.api.multipart.dispatcher.sender.response.MultipartResponse;
 import org.eclipse.edc.protocol.ids.spi.domain.IdsConstants;
 import org.eclipse.edc.protocol.ids.util.CalendarUtil;
-import org.jetbrains.annotations.NotNull;
 import sender.message.RegisterResourceMessage;
 
 import java.net.URI;
@@ -71,18 +70,20 @@ public class RegisterResourceRequestSender implements MultipartSenderDelegate<Re
                 .findFirst()
                 .orElse(Language.EN);
 
-        var version = getVersion(registerResourceMessage);
-        var keywords = getKeywords(registerResourceMessage)
+        var version = getAssetVersion(registerResourceMessage);
+        var keywords = getAssetKeywords(registerResourceMessage)
                 .stream()
                 .map(k -> new TypedLiteral(k, "en"))
                 .toList();
-        var mediaType = getMediaType(registerResourceMessage);
+        var mediaType = getAssetMediaType(registerResourceMessage);
         var publisher = getAssetPublisher(registerResourceMessage);
         var standardLicense = getAssetStandardLicense(registerResourceMessage);
         var endpointDocumentation = getAssetEndpointDocumentation(registerResourceMessage);
         var transportMode = getAssetTransportMode(registerResourceMessage);
         var dataCategory = getAssetDataCategory(registerResourceMessage);
         var dataSubcategory = getAssetDataSubcategory(registerResourceMessage);
+        var dataModel = getAssetDataModel(registerResourceMessage);
+        var geoReferenceMethod = getAssetGeoReferenceMethod(registerResourceMessage);
 
         var resource = new ResourceBuilder(registerResourceMessage.affectedResourceUri())
                 ._title_(new TypedLiteral(assetTitle, "en"))
@@ -102,15 +103,25 @@ public class RegisterResourceRequestSender implements MultipartSenderDelegate<Re
 //                        .build())
                 .build();
 
-        ObjectNode jsonNode = (ObjectNode) objectMapper.readTree(objectMapper.writeValueAsString(resource));
-        jsonNode.set("http://w3id.org/mds#transportMode", getTransportModeJson(transportMode));
-        jsonNode.set("http://w3id.org/mds#dataCategory", getDataCategory(dataCategory));
-        jsonNode.set("http://w3id.org/mds#dataSubcategory", getDataSubcategory(dataSubcategory));
-        System.out.println(jsonNode);
-        return objectMapper.writeValueAsString(jsonNode);
+        ObjectNode json = (ObjectNode) objectMapper.readTree(objectMapper.writeValueAsString(resource));
+        json.set("http://w3id.org/mds#transportMode", getTransportModeJson(transportMode));
+        json.set("http://w3id.org/mds#dataCategory", getDataCategoryJson(dataCategory));
+        json.set("http://w3id.org/mds#dataSubcategory", getDataSubcategoryJson(dataSubcategory));
+        json.set("http://w3id.org/mds#dataModel", getDataModelJson(dataModel));
+        json.set("http://w3id.org/mds#geoReferenceMethod", getGeoReferenceMethodJson(geoReferenceMethod));
+        System.out.println(json);
+        return objectMapper.writeValueAsString(json);
     }
 
-    private ObjectNode getDataSubcategory(String dataSubcategory) {
+    private ObjectNode getGeoReferenceMethodJson(String geoReferenceMethod) {
+        return getPropertyJson(geoReferenceMethod);
+    }
+
+    private ObjectNode getDataModelJson(String dataModel) {
+        return getPropertyJson(dataModel);
+    }
+
+    private ObjectNode getDataSubcategoryJson(String dataSubcategory) {
         return getPropertyJson(dataSubcategory);
     }
 
@@ -118,16 +129,23 @@ public class RegisterResourceRequestSender implements MultipartSenderDelegate<Re
         return getPropertyJson(transportMode);
     }
 
-    private ObjectNode getDataCategory(String dataCategory) {
+    private ObjectNode getDataCategoryJson(String dataCategory) {
         return getPropertyJson(dataCategory);
     }
-
 
     private ObjectNode getPropertyJson(String property) {
         ObjectNode transportModeJson = objectMapper.createObjectNode();
         transportModeJson.put("@value", property);
         transportModeJson.put("@type", "http://www.w3.org/2001/XMLSchema#string");
         return transportModeJson;
+    }
+
+    private String getAssetGeoReferenceMethod(RegisterResourceMessage registerResourceMessage) {
+        return getAssetProperty(registerResourceMessage, "http://w3id.org/mds#geoReferenceMethod");
+    }
+
+    private String getAssetDataModel(RegisterResourceMessage registerResourceMessage) {
+        return getAssetProperty(registerResourceMessage, "http://w3id.org/mds#dataModel");
     }
 
     private String getAssetDataSubcategory(RegisterResourceMessage registerResourceMessage) {
@@ -158,12 +176,12 @@ public class RegisterResourceRequestSender implements MultipartSenderDelegate<Re
         return getAssetProperty(registerResourceMessage, "asset:prop:language");
     }
 
-    private static List<String> getKeywords(RegisterResourceMessage registerResourceMessage) {
+    private static List<String> getAssetKeywords(RegisterResourceMessage registerResourceMessage) {
         var keywords = getAssetProperty(registerResourceMessage, "asset:prop:keywords");
         return new ArrayList<>(Arrays.asList(keywords.split(",")));
     }
 
-    private static String getMediaType(RegisterResourceMessage registerResourceMessage) {
+    private static String getAssetMediaType(RegisterResourceMessage registerResourceMessage) {
         return getAssetProperty(registerResourceMessage, "asset:prop:contenttype");
     }
 
@@ -175,7 +193,7 @@ public class RegisterResourceRequestSender implements MultipartSenderDelegate<Re
         return getAssetProperty(registerResourceMessage, "asset:prop:name");
     }
 
-    private static String getVersion(RegisterResourceMessage registerResourceMessage) {
+    private static String getAssetVersion(RegisterResourceMessage registerResourceMessage) {
         return getAssetProperty(registerResourceMessage, "asset:prop:version");
     }
 
