45a268aade3cba0570384cf50512485faea5bd4e
Eric Fiege <eric.fiege@sovity.de>
2023-02-01 11:14:00 +0100

    feat: Publishing a Connector-Development-Image #78

--- connector/build.gradle.kts
+++ connector/build.gradle.kts
@@ -8,34 +8,46 @@ val edcVersion: String by project
 val edcGroup: String by project
 
 dependencies {
-    implementation(project(":extensions:broker"))
-    implementation(project(":extensions:clearinghouse"))
-    implementation(project(":extensions:policy-referring-connector"))
-    implementation(project(":extensions:policy-time-interval"))
-    implementation("${edcGroup}:control-plane-core:${edcVersion}")
-    implementation("${edcGroup}:api-observability:${edcVersion}")
-    implementation("${edcGroup}:data-management-api:${edcVersion}")
-    implementation("${edcGroup}:configuration-filesystem:${edcVersion}")
-    implementation("${edcGroup}:http:${edcVersion}")
-    implementation("${edcGroup}:control-plane-aggregate-services:${edcVersion}")
 
-    // JDK Logger
-    implementation("${edcGroup}:monitor-jdk-logger:${edcVersion}")
-
-    // IDS
-    implementation("${edcGroup}:ids:${edcVersion}")
-
-    // To use FileSystem vault e.g. -DuseFsVault="true".Only for non-production usages.
-    val useFsVault: Boolean = System.getProperty("useFsVault", "true").toBoolean()
-    if (useFsVault) {
-        implementation("${edcGroup}:vault-filesystem:${edcVersion}")
+    if (project.hasProperty("dev-edc")) {
+        implementation("${edcGroup}:control-plane-core:${edcVersion}")
+        implementation("${edcGroup}:api-observability:${edcVersion}")
+        implementation("${edcGroup}:iam-mock:${edcVersion}")
+        implementation("${edcGroup}:auth-tokenbased:${edcVersion}")
+        implementation("${edcGroup}:data-management-api:${edcVersion}")
+        implementation("${edcGroup}:ids:${edcVersion}")
+        implementation(project(":extensions:contract-agreement-transfer-api"))
     } else {
-        implementation("${edcGroup}:azure-vault:${edcVersion}")
-    }
+        implementation(project(":extensions:broker"))
+        implementation(project(":extensions:clearinghouse"))
+        implementation(project(":extensions:policy-referring-connector"))
+        implementation(project(":extensions:policy-time-interval"))
+        implementation(project(":extensions:contract-agreement-transfer-api"))
+        implementation("${edcGroup}:control-plane-core:${edcVersion}")
+        implementation("${edcGroup}:api-observability:${edcVersion}")
+        implementation("${edcGroup}:data-management-api:${edcVersion}")
+        implementation("${edcGroup}:configuration-filesystem:${edcVersion}")
+        implementation("${edcGroup}:http:${edcVersion}")
+        implementation("${edcGroup}:control-plane-aggregate-services:${edcVersion}")
 
-    // Authentication for IDS API
-    implementation("${edcGroup}:oauth2-core:${edcVersion}")
-    implementation("${edcGroup}:jwt-core:${edcVersion}")
+        // JDK Logger
+        implementation("${edcGroup}:monitor-jdk-logger:${edcVersion}")
+
+        // IDS
+        implementation("${edcGroup}:ids:${edcVersion}")
+
+        // To use FileSystem vault e.g. -DuseFsVault="true".Only for non-production usages.
+        val useFsVault: Boolean = System.getProperty("useFsVault", "true").toBoolean()
+        if (useFsVault) {
+            implementation("${edcGroup}:vault-filesystem:${edcVersion}")
+        } else {
+            implementation("${edcGroup}:azure-vault:${edcVersion}")
+        }
+
+        // Authentication for IDS API
+        implementation("${edcGroup}:oauth2-core:${edcVersion}")
+        implementation("${edcGroup}:jwt-core:${edcVersion}")
+    }
 }
 
 application {
