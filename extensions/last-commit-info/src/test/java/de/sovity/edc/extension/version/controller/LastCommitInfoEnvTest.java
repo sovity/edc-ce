8202d2da63ffce613eb60de2ff8eaf22f9292194
omar <omar.silva@sovity.de>
2023-02-16 15:59:02 +0100

    chore: delete failing test

--- extensions/last-commit-info/src/test/java/de/sovity/edc/extension/version/controller/LastCommitInfoEnvTest.java
+++ extensions/last-commit-info/src/test/java/de/sovity/edc/extension/version/controller/LastCommitInfoEnvTest.java
@@ -16,18 +16,13 @@ package de.sovity.edc.extension.version.controller;
 
 import org.eclipse.edc.junit.annotations.ApiTest;
 import org.eclipse.edc.junit.extensions.EdcExtension;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-
 import static de.sovity.edc.extension.version.controller.TestUtils.createConfiguration;
 import static de.sovity.edc.extension.version.controller.TestUtils.mockRequest;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.not;
 
 @ApiTest
 @ExtendWith(EdcExtension.class)
@@ -38,32 +33,10 @@ class LastCommitInfoEnvTest {
         extension.setConfiguration(createConfiguration("env"));
     }
 
-
-
     @Test
     void testEnvAndJar() {
         var request = mockRequest();
         request.assertThat().body(containsString("pipeline"));
         request.assertThat().body(containsString("env"));
-    }
-
-    @Test
-    void testOnlyEnv() throws FileNotFoundException {
-        var classLoader = Thread.currentThread().getContextClassLoader();
-        var is = classLoader.getResourceAsStream("jar-last-commit-info.txt");
-        PrintWriter prw = new PrintWriter("src/test/resources/jar-last-commit-info.txt");
-        prw.println("These text will replace all your file content");
-        prw.close();
-
-        var request = mockRequest();
-        request.assertThat().body(not(containsString("pipeline")));
-        request.assertThat().body(containsString("env"));
-    }
-
-    @AfterAll
-    static void fixFile() throws FileNotFoundException {
-        PrintWriter prw = new PrintWriter("src/test/resources/jar-last-commit-info.txt");
-        prw.println("pipeline");
-        prw.close();
     }
 }
