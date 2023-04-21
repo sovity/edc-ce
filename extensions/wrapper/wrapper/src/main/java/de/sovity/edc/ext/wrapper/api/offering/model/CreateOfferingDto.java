80fc66026932919de399e3eb2e8476ce138c4d14
Tim Dahlmanns <tim.dahlmanns@isst.fraunhofer.de>
2023-04-21 15:58:48 +0200

    test(wrapper): Add API test for offering endpoint
    WIP: Test for empty string input

--- extensions/wrapper/wrapper/src/main/java/de/sovity/edc/ext/wrapper/api/offering/model/CreateOfferingDto.java
+++ extensions/wrapper/wrapper/src/main/java/de/sovity/edc/ext/wrapper/api/offering/model/CreateOfferingDto.java
@@ -1,16 +1,27 @@
 package de.sovity.edc.ext.wrapper.api.offering.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.eclipse.edc.connector.api.management.asset.model.AssetEntryDto;
 import org.eclipse.edc.connector.api.management.contractdefinition.model.ContractDefinitionRequestDto;
 import org.eclipse.edc.connector.api.management.policy.model.PolicyDefinitionRequestDto;
 
-@RequiredArgsConstructor
 @Getter
 public class CreateOfferingDto {
 
     private final AssetEntryDto assetEntryDto;
     private final PolicyDefinitionRequestDto policyDefinitionRequestDto;
     private final ContractDefinitionRequestDto contractDefinitionRequestDto;
+
+    @JsonCreator
+    public CreateOfferingDto(
+            @JsonProperty(value = "assetEntryDto", required = true) AssetEntryDto assetEntryDto,
+            @JsonProperty(value = "policyDefinitionRequestDto", required = true) PolicyDefinitionRequestDto policyDefinitionRequestDto,
+            @JsonProperty(value = "contractDefinitionRequestDto", required = true) ContractDefinitionRequestDto contractDefinitionRequestDto) {
+        this.assetEntryDto = assetEntryDto;
+        this.policyDefinitionRequestDto = policyDefinitionRequestDto;
+        this.contractDefinitionRequestDto = contractDefinitionRequestDto;
+    }
+
 }
