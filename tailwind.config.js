821c97eb953e3a20d4d2731b374f4fe5cd2cba95
Richard Treier <richard.treier@sovity.de>
2023-05-03 11:13:25 +0200

    chore install tailwind, remove manually added utilities (#260)

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  prefix: '',
+  purge: {
+    content: ['./src/**/*.{html,ts}'],
+  },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
