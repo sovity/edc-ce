ae7c6b3a8736590b80221cf3c79e7768ebe0bddd
Daniel Vangrieken <daniel.vangrieken@sovity.be>
2023-05-03 10:52:21 +0200

    Proposal for file upload api The delete file is removed since this should be triggered when the companion asset is removed.

--- extensions/wrapper/wrapper-ee-api/src/main/java/de/sovity/edc/ext/wrapper/api/ee/EnterpriseEditionResource.java
+++ extensions/wrapper/wrapper-ee-api/src/main/java/de/sovity/edc/ext/wrapper/api/ee/EnterpriseEditionResource.java
@@ -14,10 +14,18 @@
 
 package de.sovity.edc.ext.wrapper.api.ee;
 
-import de.sovity.edc.ext.wrapper.api.ee.model.*;
+import de.sovity.edc.ext.wrapper.api.ee.model.ConnectorLimits;
+import de.sovity.edc.ext.wrapper.api.ee.model.FileStorageUploadResult;
+import de.sovity.edc.ext.wrapper.api.ee.model.FileBlobStorage;
+import de.sovity.edc.ext.wrapper.api.ee.model.FileStorageUploadRequest;
+import de.sovity.edc.ext.wrapper.api.ee.model.FileStorageAssetCreateRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.core.MediaType;
 
 import java.util.LinkedList;
@@ -28,7 +36,6 @@ import java.util.LinkedList;
 @Path("wrapper/ee")
 @Tag(name = "Enterprise Edition", description = "EDC Enterprise Edition API Endpoints. Included here for ease of use.")
 public interface EnterpriseEditionResource {
-
     @GET
     @Path("connector-limits")
     @Produces(MediaType.APPLICATION_JSON)
@@ -36,27 +43,22 @@ public interface EnterpriseEditionResource {
     ConnectorLimits connectorLimits();
 
     @POST
-    @Path("file-storage/blobstore/files")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    FileStorageUploadResult blobStoreUploadFile(FileStorageUploadRequest request);
-
-    @DELETE
-    @Path("file-storage/blobstore/file/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    String blobStoreDeleteFile(String id);
-
-    @GET
-    @Path("file-storage/blobstore/files")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    LinkedList<FileBlobStorage> blobStoreListFiles(String id);
-
-    @POST
-    @Path("file-storage/blobstore/assets")
+    @Path("file-storage/blob-store/files")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    String createAsset(FileStorageAssetCreateRequest request);
+    @Operation(description = "Upload a file to the remote blob store.")
+    FileStorageUploadResult blobStoreUploadFile(FileStorageUploadRequest request);
 
+    @GET
+    @Path("file-storage/blob-store/files")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "Get all uploaded files stored on the remote blob store.")
+    LinkedList<FileBlobStorage> blobStoreListFiles();
+
+    @POST
+    @Path("file-storage/blob-store/assets")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(description = "Create the blob store file storage asset.")
+    FileBlobStorage createAsset(FileStorageAssetCreateRequest request);
 }
