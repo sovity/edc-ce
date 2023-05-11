1f897ef62a22e9dfb08758c3efe543e9241d87cd
Richard Treier <richard.treier@sovity.de>
2023-05-11 16:40:55 +0200

    fix swagger annotations missing in api jars

--- extensions/wrapper/wrapper-broker-api/build.gradle.kts
+++ extensions/wrapper/wrapper-broker-api/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 }
 
 dependencies {
-
     annotationProcessor("org.projectlombok:lombok:1.18.26")
     compileOnly("org.projectlombok:lombok:1.18.26")
 
@@ -12,11 +11,11 @@ dependencies {
 
     api("jakarta.ws.rs:jakarta.ws.rs-api:3.1.0")
     api("jakarta.validation:jakarta.validation-api:3.0.2")
+    api("io.swagger.core.v3:swagger-annotations-jakarta:2.2.9")
+    api("io.swagger.core.v3:swagger-jaxrs2-jakarta:2.2.9")
+    api("jakarta.servlet:jakarta.servlet-api:5.0.0")
 
     implementation("org.apache.commons:commons-lang3:3.12.0")
-    implementation("io.swagger.core.v3:swagger-annotations-jakarta:2.2.9")
-    implementation("io.swagger.core.v3:swagger-jaxrs2-jakarta:2.2.9")
-    implementation("jakarta.servlet:jakarta.servlet-api:5.0.0")
 }
 
 val sovityEdcGroup: String by project
