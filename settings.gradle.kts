4354d6c007cf04989d2b73d45f1a74738ec07481
Eric Fiege <eric.fiege@sovity.de>
2023-06-06 10:03:05 +0200

    del: ids-broker-client-Extension

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,6 @@ rootProject.name = "edc-extensions"
 
 include(":extensions:contract-agreement-transfer-api")
 include(":extensions:edc-ui-config")
-include(":extensions:ids-broker-client")
 include(":extensions:last-commit-info")
 include(":extensions:policy-always-true")
 include(":extensions:policy-referring-connector")
