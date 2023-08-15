88d6b725b067b9e316474f55ab3daa2a48e03e94
Richard Treier <richard.treier@sovity.de>
2023-08-15 16:59:08 +0200

    fix: policy mapper stub

--- extensions/wrapper/wrapper-common-mappers/src/main/java/de/sovity/edc/ext/wrapper/api/common/mappers/PolicyMapper.java
+++ extensions/wrapper/wrapper-common-mappers/src/main/java/de/sovity/edc/ext/wrapper/api/common/mappers/PolicyMapper.java
@@ -1,12 +1,12 @@
 package de.sovity.edc.ext.wrapper.api.common.mappers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import de.sovity.edc.ext.wrapper.api.common.model.PolicyDefinitionDto;
+import de.sovity.edc.ext.wrapper.api.common.model.UiPolicyCreateDto;
 import de.sovity.edc.ext.wrapper.api.common.model.UiPolicyDto;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.eclipse.edc.connector.policy.spi.PolicyDefinition;
 import org.eclipse.edc.policy.model.Policy;
+import org.eclipse.edc.policy.model.PolicyType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,27 +21,15 @@ public class PolicyMapper {
     public UiPolicyDto buildPolicyDto(Policy policy) {
         return UiPolicyDto.builder()
                 .policyJsonLd(serializePolicy(policy))
+                // TODO fully implement
                 .constraints(List.of())
                 .errors(new ArrayList<>())
                 .build();
     }
 
-    public PolicyDefinitionDto buildPolicyDefinitionDto(PolicyDefinition policyDefinition) {
-        return PolicyDefinitionDto.builder().uiPolicyDto(buildPolicyDto(policyDefinition.getPolicy())).build();
-    }
-
-
-    public Policy buildPolicy(UiPolicyDto policyDto) {
-        return deserializePolicy(policyDto.getPolicyJsonLd());
-    }
-
-    public PolicyDefinition buildPolicyDefinition(PolicyDefinitionDto policyDefinitionDto) {
-        return PolicyDefinition.Builder.newInstance().policy(buildPolicy(policyDefinitionDto.getUiPolicyDto())).build();
-    }
-
-    @SneakyThrows
-    public Policy deserializePolicy(String edcPolicyJsonLd) {
-        return jsonLdObjectMapper.readValue(edcPolicyJsonLd, Policy.class);
+    public Policy buildPolicy(UiPolicyCreateDto policyCreateDto) {
+        // TODO fully implement
+        return Policy.Builder.newInstance().type(PolicyType.SET).build();
     }
 
     @SneakyThrows
