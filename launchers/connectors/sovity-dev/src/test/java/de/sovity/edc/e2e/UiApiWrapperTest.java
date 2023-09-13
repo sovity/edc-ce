8b5491e4c89ffaa4de1672b733f33ada17a99700
Richard Treier <richard.treier@sovity.de>
2023-09-13 14:05:55 +0200

    wip

---
+++ launchers/connectors/sovity-dev/src/test/java/de/sovity/edc/e2e/UiApiWrapperTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2023 sovity GmbH
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *      sovity GmbH - init
+ */
+
+package de.sovity.edc.e2e;
+
+import de.sovity.edc.extension.e2e.connector.ConnectorRemote;
+import de.sovity.edc.extension.e2e.connector.MockDataAddressRemote;
+import de.sovity.edc.extension.e2e.db.TestDatabase;
+import de.sovity.edc.extension.e2e.db.TestDatabaseFactory;
+import org.eclipse.edc.junit.extensions.EdcExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import static de.sovity.edc.extension.e2e.connector.DataTransferTestUtil.validateDataTransferred;
+import static de.sovity.edc.extension.e2e.connector.config.ConnectorConfigFactory.forTestDatabase;
+import static de.sovity.edc.extension.e2e.connector.config.ConnectorRemoteConfigFactory.fromConnectorConfig;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UiApiWrapperTest {
+
+    private static final String PROVIDER_PARTICIPANT_ID = "provider";
+    private static final String CONSUMER_PARTICIPANT_ID = "consumer";
+    private static final String TEST_BACKEND_TEST_DATA = UUID.randomUUID().toString();
+
+    @RegisterExtension
+    static EdcExtension providerEdcContext = new EdcExtension();
+    @RegisterExtension
+    static EdcExtension consumerEdcContext = new EdcExtension();
+
+    @RegisterExtension
+    static final TestDatabase PROVIDER_DATABASE = TestDatabaseFactory.getTestDatabase(1);
+    @RegisterExtension
+    static final TestDatabase CONSUMER_DATABASE = TestDatabaseFactory.getTestDatabase(2);
+
+    private ConnectorRemote providerConnector;
+    private ConnectorRemote consumerConnector;
+    private MockDataAddressRemote dataAddress;
+
+    @BeforeEach
+    void setup() {
+        var providerConfig = forTestDatabase(PROVIDER_PARTICIPANT_ID, 22000, PROVIDER_DATABASE);
+        providerEdcContext.setConfiguration(providerConfig.getProperties());
+        providerConnector = new ConnectorRemote(fromConnectorConfig(providerConfig));
+
+        var consumerConfig = forTestDatabase(CONSUMER_PARTICIPANT_ID, 23000, CONSUMER_DATABASE);
+        consumerEdcContext.setConfiguration(consumerConfig.getProperties());
+        consumerConnector = new ConnectorRemote(fromConnectorConfig(consumerConfig));
+
+        // We use the provider EDC as data sink / data source (it has the test-backend-controller extension)
+        dataAddress = new MockDataAddressRemote(providerConnector.getConfig().getDefaultEndpoint());
+    }
+
+    @Test
+    void testContractNegotiation() {
+        // arrange
+        var assetId = UUID.randomUUID().toString();
+        providerConnector.createDataOffer(assetId, dataAddress.getDataSourceUrl(TEST_BACKEND_TEST_DATA));
+
+        // act
+        consumerConnector.consumeOffer(
+                providerConnector.getParticipantId(),
+                providerConnector.getConfig().getProtocolEndpoint().getUri(),
+                assetId,
+                dataAddress.getDataSinkJsonLd());
+
+        // assert
+        validateDataTransferred(dataAddress.getDataSinkSpyUrl(), TEST_BACKEND_TEST_DATA);
+    }
+}
