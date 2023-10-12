6d2abc02546cd228a2e53962a1a7d5b5460f0570
Richard Treier <richard.treier@sovity.de>
2023-10-12 10:42:49 +0200

    test: asset null behavior (#269)

--- extensions/broker-server/src/test/java/de/sovity/edc/ext/brokerserver/services/refreshing/ConnectorUpdaterTest.java
+++ extensions/broker-server/src/test/java/de/sovity/edc/ext/brokerserver/services/refreshing/ConnectorUpdaterTest.java
@@ -31,6 +31,7 @@ import org.eclipse.edc.junit.annotations.ApiTest;
 import org.eclipse.edc.junit.extensions.EdcExtension;
 import org.eclipse.edc.policy.model.Policy;
 import org.eclipse.edc.spi.asset.AssetSelectorExpression;
+import org.eclipse.edc.spi.persistence.EdcPersistenceException;
 import org.eclipse.edc.spi.types.domain.DataAddress;
 import org.eclipse.edc.spi.types.domain.asset.Asset;
 import org.junit.jupiter.api.BeforeEach;
@@ -45,6 +46,7 @@ import java.util.Map;
 
 import static de.sovity.edc.ext.brokerserver.TestUtils.createConfiguration;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ApiTest
 @ExtendWith(EdcExtension.class)
@@ -66,13 +68,44 @@ class ConnectorUpdaterTest {
     ) {
         TEST_DATABASE.testTransaction(dsl -> {
             // arrange
+            var connectorEndpoint = TestUtils.PROTOCOL_ENDPOINT;
             var connectorUpdater = BrokerServerExtensionContext.instance.connectorUpdater();
             var connectorCreator = BrokerServerExtensionContext.instance.connectorCreator();
-            String connectorEndpoint = TestUtils.PROTOCOL_ENDPOINT;
 
             createAlwaysTruePolicyDefinition(policyDefinitionStore);
             createAlwaysTrueContractDefinition(contractDefinitionStore);
-            createAsset(assetService, "test-asset-1", "Test Asset 1");
+            
+            var nestedObjProperty = new LinkedHashMap<String, Object>(Map.of(
+                    "test-string", "hello",
+                    "test-uri", "https://w3id.org/idsa/code/AB",
+                    "http://test-uri-key", "value",
+
+                    "test-array", new ArrayList<>(List.of("a", "b")),
+                    "test-float", 5.1,
+                    "test-int", 5,
+                    "test-boolean", true,
+
+                    "test-obj", new LinkedHashMap<>(Map.of("key", "value"))
+            ));
+            nestedObjProperty.put("test-null", null);
+
+            var asset = Asset.Builder.newInstance()
+                    .id("test-asset-1")
+                    .property(AssetProperty.ASSET_ID, "test-asset-1")
+                    .property(AssetProperty.ASSET_NAME, "Test Asset 1")
+                    .property("test-string", "hello")
+                    .property("test-uri", "https://w3id.org/idsa/code/AB")
+                    .property("http://test-uri-key", "value")
+
+                    .property("test-array", new ArrayList<>(List.of("a", "b")))
+                    .property("test-float", 5.1)
+                    .property("test-int", 5)
+                    .property("test-boolean", true)
+
+                    .property("test-obj", nestedObjProperty)
+                    .build();
+
+            assetService.create(asset, dataAddress());
             connectorCreator.addConnector(dsl, connectorEndpoint);
 
             // act
@@ -92,23 +125,54 @@ class ConnectorUpdaterTest {
 
             var props = JsonPath.from(dataOffer.getAssetProperties().data());
             assertThat(props.getString("\"asset:prop:name\"")).isEqualTo("Test Asset 1");
+            assertThat(props.getString("test-string")).isEqualTo("hello");
+            assertThat(props.getString("test-uri")).isEqualTo("https://w3id.org/idsa/code/AB");
+            assertThat(props.getString("http://test-uri-key")).isEqualTo("value");
+
             assertThat(props.getString("test-array")).isEqualTo("[\"a\",\"b\"]");
             assertThat(props.getString("test-int")).isEqualTo("5");
-            assertThat(props.getString("test-double")).isEqualTo("5.1");
+            assertThat(props.getString("test-float")).isEqualTo("5.1");
             assertThat(props.getString("test-boolean")).isEqualTo("true");
-            assertThat(props.getString("test-uri")).isEqualTo("https://w3id.org/idsa/code/AB");
 
             var testObj = JsonPath.from(props.getString("test-obj"));
-            assertThat((Map<?, ?>) testObj.get("test-obj")).isEqualTo(Map.of("key", "value"));
+            assertThat((String) testObj.get("test-string")).isEqualTo("hello");
+            assertThat((String) testObj.get("test-uri")).isEqualTo("https://w3id.org/idsa/code/AB");
+            assertThat((String) testObj.get("http://test-uri-key")).isEqualTo("value");
+
             assertThat((List<?>) testObj.get("test-array")).isEqualTo(List.of("a", "b"));
             assertThat((Integer) testObj.get("test-int")).isEqualTo(5);
-            assertThat((Float) testObj.get("test-double")).isEqualTo(5.1f);
+            assertThat((Float) testObj.get("test-float")).isEqualTo(5.1f);
             assertThat((Boolean) testObj.get("test-boolean")).isTrue();
-            assertThat((String) testObj.get("test-uri")).isEqualTo("https://w3id.org/idsa/code/AB");
+            assertThat((Map<?, ?>) testObj.get("test-obj")).isEqualTo(Map.of("key", "value"));
+
+            // the nested object's null will have disappeared
+            assertThat(testObj.getMap("")).containsKey("test-string");
+            assertThat(testObj.getMap("")).doesNotContainKey("test-null");
 
             var contractOffers = dsl.selectFrom(Tables.DATA_OFFER_CONTRACT_OFFER).stream().toList();
             assertThat(contractOffers).hasSize(1);
         });
+    }
+
+    @Test
+    void testTopLevelAssetPropertyCannotBeNull(AssetService assetService) {
+        var asset = Asset.Builder.newInstance()
+                .id("test-asset-1")
+                .property("test-null", null)
+                .build();
+        var dataAddress = dataAddress();
+        assertThatThrownBy(() -> assetService.create(asset, dataAddress))
+                .isInstanceOf(EdcPersistenceException.class)
+                .hasMessage("java.lang.NullPointerException: Cannot invoke \"Object.getClass()\" because the return value of \"java.util.Map$Entry.getValue()\" is null");
+    }
+
+    private DataAddress dataAddress() {
+        return DataAddress.Builder.newInstance()
+                .properties(Map.of(
+                        "type", "HttpData",
+                        "baseUrl", "https://jsonplaceholder.typicode.com/todos/1"
+                ))
+                .build();
     }
 
     private void createAlwaysTruePolicyDefinition(PolicyDefinitionStore policyDefinitionStore) {
@@ -130,41 +194,4 @@ class ConnectorUpdaterTest {
         contractDefinitionStore.save(contractDefinition);
     }
 
-    private void createAsset(
-            AssetService assetService,
-            String assetId,
-            String assetName
-    ) {
-        var asset = Asset.Builder.newInstance()
-                .id(assetId)
-                .property(AssetProperty.ASSET_ID, assetId)
-                .property(AssetProperty.ASSET_NAME, assetName)
-                .property("test-uri", "https://w3id.org/idsa/code/AB")
-                .property("http://test-uri-key", "value")
-                .property("test-obj", new LinkedHashMap<>(Map.of(
-                        "test-uri", "https://w3id.org/idsa/code/AB",
-                        "http://test-uri-key", "value",
-                        "test-obj", new LinkedHashMap<>(Map.of(
-                                "key", "value"
-                        )),
-                        "test-array", new ArrayList<>(List.of("a", "b")),
-                        "test-string", "hello",
-                        "test-int", 5,
-                        "test-double", 5.1,
-                        "test-boolean", true
-                )))
-                .property("test-array", new ArrayList<>(List.of("a", "b")))
-                .property("test-string", "hello")
-                .property("test-int", 5)
-                .property("test-double", 5.1)
-                .property("test-boolean", true)
-                .build();
-        var dataAddress = DataAddress.Builder.newInstance()
-                .properties(Map.of(
-                        "type", "HttpData",
-                        "baseUrl", "https://jsonplaceholder.typicode.com/todos/1"
-                ))
-                .build();
-        assetService.create(asset, dataAddress);
-    }
 }
