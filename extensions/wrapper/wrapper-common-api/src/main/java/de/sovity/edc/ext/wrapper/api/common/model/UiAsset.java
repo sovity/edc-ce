12f675f68aff2dbd113b3372e34bcec22f147f19
kulgg <75735874+kulgg@users.noreply.github.com>
2024-01-22 16:17:34 +0100

    feat: new MDS Asset properties, initial version (#736)

--- extensions/wrapper/wrapper-common-api/src/main/java/de/sovity/edc/ext/wrapper/api/common/model/UiAsset.java
+++ extensions/wrapper/wrapper-common-api/src/main/java/de/sovity/edc/ext/wrapper/api/common/model/UiAsset.java
@@ -22,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -106,6 +107,36 @@ public class UiAsset {
 
     @Schema(description = "Transport Mode", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String transportMode;
+
+    @Schema(description = "The sovereign is distinct from the publisher by being the legal owner of the data.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private String sovereignLegalName;
+
+    @Schema(description = "Geo location", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private String geoLocation;
+
+    @Schema(description = "Locations by NUTS standard which divides countries into administrative divisions", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private List<String> nutsLocation;
+
+    @Schema(description = "Data sample URLs", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private List<String> dataSampleUrls;
+
+    @Schema(description = "Reference file/schema URLs", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private List<String> referenceFileUrls;
+
+    @Schema(description = "Additional information on reference files/schemas", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private String referenceFilesDescription;
+
+    @Schema(description = "Instructions for use that are not legally relevant e.g. information on how to cite the dataset in papers", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private String conditionsForUse;
+
+    @Schema(description = "Data update frequency", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private String dataUpdateFrequency;
+
+    @Schema(description = "Temporal coverage start date", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private LocalDate temporalCoverageFrom;
+
+    @Schema(description = "Temporal coverage end date (inclusive)", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private LocalDate temporalCoverageToInclusive;
 
     @Schema(description = "Unhandled Asset Properties (that were strings)", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private Map<String, String> additionalProperties;
