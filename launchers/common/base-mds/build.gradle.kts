4b13a984a89b01f8a3030421b17ddce40b9a2307
dhommen <dh@orbiter.de>
2024-01-23 16:48:40 +0100

    chore(logging-house-extension): adjusting surrounding comments

--- launchers/common/base-mds/build.gradle.kts
+++ launchers/common/base-mds/build.gradle.kts
@@ -3,11 +3,6 @@ plugins {
 }
 
 dependencies {
-    // MDS Related EDC Extensions
-    // [...]
-
-    // Currently there are no mds-specific EDC extensions
-    // The MDS broker and MDS clearing house extensions are in the process of being migrated to the DSP protocol
     implementation("logging-house:logging-house-client:0.1.0")
 }
 
