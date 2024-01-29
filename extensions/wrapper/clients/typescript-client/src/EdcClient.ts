96e37c167352eb0bbb156670ed4ae182191dd16e
kulgg <75735874+kulgg@users.noreply.github.com>
2024-01-29 11:25:42 +0100

    feat: add OAuth client credentials option to TS client

--- extensions/wrapper/clients/typescript-client/src/EdcClient.ts
+++ extensions/wrapper/clients/typescript-client/src/EdcClient.ts
@@ -1,10 +1,7 @@
-import {
-    Configuration,
-    ConfigurationParameters,
-    EnterpriseEditionApi,
-    UIApi,
-    UseCaseApi,
-} from './generated';
+import {Configuration, ConfigurationParameters, EnterpriseEditionApi, UIApi, UseCaseApi,} from './generated';
+import {OAuth2ClientCredentials} from "./oauth2/OAuth2ClientCredentials";
+import {OAuth2Client, OAuth2Fetch} from "@badgateway/oauth2-client";
+
 
 /**
  * API Client for our sovity EDC
@@ -20,12 +17,30 @@ export interface EdcClient {
  * @param opts opts
  */
 export function buildEdcClient(opts: EdcClientOptions): EdcClient {
+    let fetchWrapper: OAuth2Fetch | null = null;
+    if (opts.oAuth2ClientCredentials) {
+        const client = new OAuth2Client({
+            server: opts.oAuth2ClientCredentials.serverUrl,
+            tokenEndpoint: opts.oAuth2ClientCredentials.tokenEndpoint,
+            clientId: opts.oAuth2ClientCredentials.clientId,
+            clientSecret: opts.oAuth2ClientCredentials.clientSecret,
+        });
+        // Wrapper for fetch that automatically sets Authorization: Bearer headers and refreshes the token when needed
+        fetchWrapper = new OAuth2Fetch({
+            client: client,
+            getNewToken: async () => {
+                return client.clientCredentials();
+            },
+        });
+    }
+
     const config = new Configuration({
         basePath: opts.managementApiUrl,
         headers: {
             'X-Api-Key': opts.managementApiKey ?? 'ApiKeyDefaultValue',
         },
         credentials: 'same-origin',
+        fetchApi: fetchWrapper ? fetchWrapper.fetch : undefined,
         ...opts.configOverrides,
     });
 
@@ -42,5 +57,6 @@ export function buildEdcClient(opts: EdcClientOptions): EdcClient {
 export interface EdcClientOptions {
     managementApiUrl: string;
     managementApiKey?: string;
+    oAuth2ClientCredentials?: OAuth2ClientCredentials,
     configOverrides?: Partial<ConfigurationParameters>;
 }
---
+++ extensions/wrapper/clients/typescript-client/src/oauth2/OAuth2ClientCredentials.ts
@@ -0,0 +1,6 @@
+export interface OAuth2ClientCredentials {
+     serverUrl: string;
+     tokenEndpoint: string;
+     clientId: string;
+     clientSecret: string;
+}
