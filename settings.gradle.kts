654510c1e5d3af6285986166d4632757621cbc21
Christophe Loiseau <christophe.loiseau@sovity.de>
2024-05-02 18:00:07 +0200

    Clearing

--- settings.gradle.kts
+++
@@ -1,7 +0,0 @@
-rootProject.name = "edc-broker-server-extension"
-
-include(":extensions:broker-server")
-include(":extensions:broker-server-postgres-flyway-jooq")
-include(":extensions:broker-server-api:api")
-include(":extensions:broker-server-api:client")
-include(":connector")
