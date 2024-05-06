d867f9d74fde42d269432caa4b5ec8bc82fc1cc6
Christophe Loiseau <christophe.loiseau@sovity.de>
2024-05-06 18:09:50 +0200

    Add connector submodule

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 rootProject.name = "edc-extensions"
 
+include(":connector")
 include(":extensions:broker-server")
 include(":extensions:broker-server-api:api")
 include(":extensions:broker-server-api:client")
