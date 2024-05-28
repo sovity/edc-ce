36b10c932c21083c6f99cb2e70aea227ebc489d5
Tim Berthold <75306992+tmberthold@users.noreply.github.com>
2024-05-28 09:55:07 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,18 @@ tasks {
     }
 }
 
+// setting duplicatesStrategy to EXCLUDE to avoid build errors caused by duplicate entries in the JAR file
+// this ensures that if a duplicate file is found, it will be excluded from the resulting JAR
+tasks.withType<Jar> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+// setting duplicatesStrategy to EXCLUDE for any copy tasks to prevent errors during the build process due to duplicate files
+// this ensures a smooth build by excluding duplicates
+tasks.withType<Copy> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
 allprojects {
     apply(plugin = "java")
     apply(plugin = "checkstyle")
