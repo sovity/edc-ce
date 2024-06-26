d6971821be2f13c8077b42b85ad7b18d0daf390c
Richard Treier <richard.treier@sovity.de>
2024-06-26 09:37:22 +0200

    fix ApiInformation

--- extensions/wrapper/wrapper-api/src/main/java/de/sovity/edc/ext/wrapper/api/ApiInformation.java
+++ extensions/wrapper/wrapper-api/src/main/java/de/sovity/edc/ext/wrapper/api/ApiInformation.java
@@ -26,16 +26,16 @@ import io.swagger.v3.oas.annotations.servers.Server;
                 title = "sovity EDC API Wrapper",
                 version = "0.0.0",
                 description = "sovity's EDC API Wrapper contains a selection of APIs for multiple consumers, " +
-                        "e.g. our EDC UI API, our generic Use Case API, our Commercial APIs, etc. " +
+                        "e.g. our EDC UI API, our generic Use Case API, our Commercial Edition APIs, etc. " +
                         "We bundled these APIs, so we can have an easier time generating our API Client Libraries.",
                 contact = @Contact(
-                        name = "Sovity GmbH",
+                        name = "sovity GmbH",
                         email = "contact@sovity.de",
-                        url = "https://github.com/sovity/edc-extensions/issues/new/choose"
+                        url = "https://github.com/sovity/edc-ce/issues/new/choose"
                 ),
                 license = @License(
                         name = "Apache 2.0",
-                        url = "https://github.com/sovity/edc-extensions/blob/main/LICENSE"
+                        url = "https://github.com/sovity/edc-ce/blob/main/LICENSE"
                 )
         ),
         servers = {
@@ -43,7 +43,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
         },
         externalDocs = @ExternalDocumentation(
                 description = "EDC API Wrapper Project in sovity/edc-extensions",
-                url = "https://github.com/sovity/edc-extensions/tree/main/extensions/wrapper"
+                url = "https://github.com/sovity/edc-ce/tree/main/extensions/wrapper"
         )
 )
 public interface ApiInformation {
