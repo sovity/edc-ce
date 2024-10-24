acc35ba44ea003800fe527383fd78c1b3d4d8b7b
Christophe Loiseau <christophe.loiseau@sovity.de>
2024-10-24 18:17:21 +0200

    Explicit individual test runnning

--- partial_validator.sh
+++ partial_validator.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 gradle \
-    :extensions:test \
+    :extensions:contract-termination:test \
+    :extensions:edc-ui-config:test \
+    :extensions:last-commit-info:test \
+    :extensions:policy-always-true:test \
+    :extensions:policy-referring-connector:test \
+    :extensions:sovity-messenger:test \
     :utils:catalog-parser:test \
     :tests:test \
+    --tests 'de.sovity.edc.e2e.wrapper.RecreateAlwaysTruePolicyTest' \
     --tests 'de.sovity.edc.e2e.ApiWrapperDemoTest' \
     --tests 'de.sovity.edc.e2e.AssetApiServiceTest' \
     --tests 'de.sovity.edc.e2e.CatalogApiTest' \
