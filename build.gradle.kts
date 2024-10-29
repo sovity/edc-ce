a68b21fec7033cdc2c7f07e64a4ba08248b4a1d9
Christophe Loiseau <christophe.loiseau@sovity.de>
2024-10-29 16:19:52 +0100

    Stop forcing the 0.2.1 medtadata model

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,10 +16,6 @@ allprojects {
     apply(plugin = "java")
     apply(plugin = "checkstyle")
 
-    configurations.all {
-        resolutionStrategy.force("org.eclipse.edc:runtime-metamodel:0.2.1")
-    }
-
     tasks.withType<JavaCompile> {
         options.encoding = "UTF-8"
         sourceCompatibility = JavaVersion.VERSION_17.toString()
